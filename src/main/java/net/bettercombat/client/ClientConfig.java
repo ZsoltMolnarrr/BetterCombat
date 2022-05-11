cc4b20f0f605dfa38b63ca1e0c626085ae7c1084
Zsolt Molnar <molnar.zsolt@magyarbankholding.hu>
2022-05-11 16:13:56 +0200

    Add config translations

--- src/main/java/net/bettercombat/client/ClientConfig.java
+++ src/main/java/net/bettercombat/client/ClientConfig.java
@@ -1,11 +1,11 @@
 package net.bettercombat.client;
 
-import me.lortseam.completeconfig.api.ConfigContainer;
 import me.lortseam.completeconfig.api.ConfigEntries;
+import me.lortseam.completeconfig.api.ConfigGroup;
 import net.minecraft.client.util.InputUtil;
 
 @ConfigEntries
-public class ClientConfig implements ConfigContainer {
+public class ClientConfig implements ConfigGroup {
     public boolean isHoldToAttackEnabled = true;
     public boolean isMiningWithWeaponsEnabled = true;
     public boolean isSwingThruGrassEnabled = true;
