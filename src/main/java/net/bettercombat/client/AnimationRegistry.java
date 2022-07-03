6ea7395a40cb80ee1befab72d0d958059f02d0c8
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2022-07-03 20:57:37 +0200

    Start migration

--- src/main/java/net/bettercombat/client/AnimationRegistry.java
+++ src/main/java/net/bettercombat/client/AnimationRegistry.java
@@ -1,7 +1,7 @@
 package net.bettercombat.client;
 
-import io.github.kosmx.emotes.common.emote.EmoteData;
-import io.github.kosmx.emotes.server.serializer.UniversalEmoteSerializer;
+import dev.kosmx.playerAnim.core.data.KeyframeAnimation;
+import dev.kosmx.playerAnim.core.data.gson.AnimationSerializing;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
 
@@ -9,24 +9,24 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class AnimationRegistry {
-    public static Map<String, EmoteData> emotes = new HashMap<>();
+public class AnimationRegistry { // TODO - Call at client init
+    public static Map<String, KeyframeAnimation> emotes = new HashMap<>(); // TODO - Rename
 
     public static void load(ResourceManager resourceManager) {
         var dataFolder = "weapon_animations";
         for (Identifier identifier : resourceManager.findResources(dataFolder, fileName -> fileName.endsWith(".json"))) {
             try {
                 var resource = resourceManager.getResource(identifier);
-                List<EmoteData> emotes = UniversalEmoteSerializer.readData(resource.getInputStream(), null, "json");
-                EmoteData emote = emotes.get(0);
-                emote.isBuiltin = true;
-                emote.bodyParts.get("head").pitch.isEnabled = false;
+                List<KeyframeAnimation> animations = AnimationSerializing.deserializeAnimation(resource.getInputStream());
+                KeyframeAnimation animation = emotes.get(0);
+                animation.isBuiltin = true;
+                // animation.bodyParts.get("head").pitch.isEnabled = false;
 
                 var id = identifier
                         .toString()
                         .replace(dataFolder + "/", "");
                 id = id.substring(0, id.lastIndexOf('.'));
-                AnimationRegistry.emotes.put(id, emote);
+                AnimationRegistry.emotes.put(id, animation);
             } catch (Exception e) {
                 e.printStackTrace();
             }
--- src/main/java/net/bettercombat/mixin/AbstractClientPlayerEntityMixin.java
+++ src/main/java/net/bettercombat/mixin/AbstractClientPlayerEntityMixin.java
@@ -1,11 +1,12 @@
 package net.bettercombat.mixin;
 
 import com.mojang.authlib.GameProfile;
-import io.github.kosmx.emotes.common.emote.EmoteData;
-import io.github.kosmx.playerAnim.IAnimatedPlayer;
-import io.github.kosmx.playerAnim.layered.AnimationContainer;
-import io.github.kosmx.playerAnim.layered.EmoteDataPlayer;
-import io.github.kosmx.playerAnim.layered.IAnimation;
+import dev.kosmx.playerAnim.api.layered.AnimationContainer;
+import dev.kosmx.playerAnim.api.layered.IAnimation;
+import dev.kosmx.playerAnim.api.layered.KeyframeAnimationPlayer;
+import dev.kosmx.playerAnim.core.data.KeyframeAnimation;
+import dev.kosmx.playerAnim.core.impl.AnimationPlayer;
+import dev.kosmx.playerAnim.impl.IAnimatedPlayer;
 import net.bettercombat.logic.WeaponRegistry;
 import net.bettercombat.client.AnimationRegistry;
 import net.bettercombat.client.PlayerAttackAnimatable;
@@ -20,14 +21,13 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import javax.annotation.Nullable;
-import java.util.Objects;
 
 @Mixin(AbstractClientPlayerEntity.class)
 public abstract class AbstractClientPlayerEntityMixin extends PlayerEntity implements PlayerAttackAnimatable {
 
-    private AnimationContainer posePlayer = new AnimationContainer(null);
+    private AnimationContainer posePlayer = new AnimationContainer(null); // TODO - Rename `Player`
     private AnimationContainer attackPlayer = new AnimationContainer(null);
-    private EmoteData lastPose;
+    private KeyframeAnimation lastPose;
 
     public AbstractClientPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile profile) {
         super(world, pos, yaw, profile);
@@ -47,7 +47,7 @@ public abstract class AbstractClientPlayerEntityMixin extends PlayerEntity imple
     public void updateAnimationsOnTick() {
         var instance = (Object)this;
         var player = (PlayerEntity)instance;
-        EmoteData newPose = null;
+        KeyframeAnimation newPose = null;
         if (player.handSwinging) {
             setPose(newPose);
             return;
@@ -59,7 +59,7 @@ public abstract class AbstractClientPlayerEntityMixin extends PlayerEntity imple
         setPose(newPose);
     }
 
-    private void setPose(@Nullable EmoteData pose) {
+    private void setPose(@Nullable KeyframeAnimation pose) {
         if (pose == lastPose) {
             return;
         }
@@ -68,8 +68,8 @@ public abstract class AbstractClientPlayerEntityMixin extends PlayerEntity imple
             this.posePlayer.setAnim(null);
         } else {
             var copy = pose.mutableCopy().build();
-            updateAnimationByCurrentActivity(copy);
-            this.posePlayer.setAnim(new EmoteDataPlayer(copy, 0));
+//            updateAnimationByCurrentActivity(copy);
+            this.posePlayer.setAnim(new KeyframeAnimationPlayer(copy, 0));
         }
 
         lastPose = pose;
@@ -78,61 +78,61 @@ public abstract class AbstractClientPlayerEntityMixin extends PlayerEntity imple
     @Override
     public void playAttackAnimation(String name, boolean isOffHand, float length) {
         try {
-            EmoteData animation = AnimationRegistry.emotes.get(name).mutableCopy().build();
-            updateAnimationByCurrentActivity(animation);
-            attackPlayer.setAnim(new EmoteDataPlayer(animation, 0));
+            KeyframeAnimation animation = AnimationRegistry.emotes.get(name).mutableCopy().build();
+//            updateAnimationByCurrentActivity(animation);
+            attackPlayer.setAnim(new KeyframeAnimationPlayer(animation, 0));
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void updateAnimationByCurrentActivity(EmoteData animation) {
-        var pose = getPose();
-        switch (pose) {
-            case STANDING -> {
-            }
-            case FALL_FLYING -> {
-            }
-            case SLEEPING -> {
-            }
-            case SWIMMING -> {
-                configurBodyPart(animation.bodyParts.get("rightLeg"), false, false);
-                configurBodyPart(animation.bodyParts.get("leftLeg"), false, false);
-            }
-            case SPIN_ATTACK -> {
-            }
-            case CROUCHING -> {
-                configurBodyPart(animation.bodyParts.get("head"), true, false);
-                configurBodyPart(animation.bodyParts.get("rightArm"), true, false);
-                configurBodyPart(animation.bodyParts.get("leftArm"), true, false);
-                configurBodyPart(animation.bodyParts.get("rightLeg"), false, false);
-                configurBodyPart(animation.bodyParts.get("leftLeg"), false, false);
-            }
-            case LONG_JUMPING -> {
-            }
-            case DYING -> {
-            }
-        }
-        if (isMounting()) {
-            configurBodyPart(animation.bodyParts.get("rightLeg"), false, false);
-            configurBodyPart(animation.bodyParts.get("leftLeg"), false, false);
-        }
-    }
+//    private void updateAnimationByCurrentActivity(EmoteData animation) {
+//        var pose = getPose();
+//        switch (pose) {
+//            case STANDING -> {
+//            }
+//            case FALL_FLYING -> {
+//            }
+//            case SLEEPING -> {
+//            }
+//            case SWIMMING -> {
+//                configurBodyPart(animation.bodyParts.get("rightLeg"), false, false);
+//                configurBodyPart(animation.bodyParts.get("leftLeg"), false, false);
+//            }
+//            case SPIN_ATTACK -> {
+//            }
+//            case CROUCHING -> {
+//                configurBodyPart(animation.bodyParts.get("head"), true, false);
+//                configurBodyPart(animation.bodyParts.get("rightArm"), true, false);
+//                configurBodyPart(animation.bodyParts.get("leftArm"), true, false);
+//                configurBodyPart(animation.bodyParts.get("rightLeg"), false, false);
+//                configurBodyPart(animation.bodyParts.get("leftLeg"), false, false);
+//            }
+//            case LONG_JUMPING -> {
+//            }
+//            case DYING -> {
+//            }
+//        }
+//        if (isMounting()) {
+//            configurBodyPart(animation.bodyParts.get("rightLeg"), false, false);
+//            configurBodyPart(animation.bodyParts.get("leftLeg"), false, false);
+//        }
+//    }
 
-    private static void configurBodyPart(EmoteData.StateCollection bodyPart, boolean isRotationEnabled, boolean isOffsetEnabled) {
-        bodyPart.pitch.isEnabled = isRotationEnabled;
-        bodyPart.roll.isEnabled = isRotationEnabled;
-        bodyPart.yaw.isEnabled = isRotationEnabled;
-        bodyPart.x.isEnabled = isOffsetEnabled;
-        bodyPart.y.isEnabled = isOffsetEnabled;
-        bodyPart.z.isEnabled = isOffsetEnabled;
-    }
+//    private static void configurBodyPart(EmoteData.StateCollection bodyPart, boolean isRotationEnabled, boolean isOffsetEnabled) {
+//        bodyPart.pitch.isEnabled = isRotationEnabled;
+//        bodyPart.roll.isEnabled = isRotationEnabled;
+//        bodyPart.yaw.isEnabled = isRotationEnabled;
+//        bodyPart.x.isEnabled = isOffsetEnabled;
+//        bodyPart.y.isEnabled = isOffsetEnabled;
+//        bodyPart.z.isEnabled = isOffsetEnabled;
+//    }
 
     @Override
     public void stopAttackAnimation() {
         IAnimation currentAnimation = attackPlayer.getAnim();
-        if (currentAnimation != null && currentAnimation instanceof EmoteDataPlayer) {
-            ((EmoteDataPlayer)currentAnimation).stop();
+        if (currentAnimation != null && currentAnimation instanceof AnimationPlayer) {
+            // ((AnimationPlayer)currentAnimation).stop(); // TODO - Missing from API ?
         }
     }
 
