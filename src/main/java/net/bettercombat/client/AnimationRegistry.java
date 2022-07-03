b0236232c0b4f48d464c7e341d4f6f8dd9319e98
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2022-07-03 21:55:37 +0200

    Update invalid pose resources

--- src/main/java/net/bettercombat/client/AnimationRegistry.java
+++ src/main/java/net/bettercombat/client/AnimationRegistry.java
@@ -1,15 +1,18 @@
 package net.bettercombat.client;
 
+import com.mojang.logging.LogUtils;
 import dev.kosmx.playerAnim.core.data.KeyframeAnimation;
 import dev.kosmx.playerAnim.core.data.gson.AnimationSerializing;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class AnimationRegistry {
+    static final Logger LOGGER = LogUtils.getLogger();
     public static Map<String, KeyframeAnimation> emotes = new HashMap<>(); // TODO - Rename
 
     public static void load(ResourceManager resourceManager) {
@@ -19,7 +22,6 @@ public class AnimationRegistry {
                 var resource = resourceManager.getResource(identifier);
                 List<KeyframeAnimation> animations = AnimationSerializing.deserializeAnimation(resource.getInputStream());
                 KeyframeAnimation animation = animations.get(0);
-                // animation.bodyParts.get("head").pitch.isEnabled = false;
 
                 var id = identifier
                         .toString()
@@ -27,6 +29,7 @@ public class AnimationRegistry {
                 id = id.substring(0, id.lastIndexOf('.'));
                 AnimationRegistry.emotes.put(id, animation);
             } catch (Exception e) {
+                LOGGER.error("Failed to load animation " + identifier.toString());
                 e.printStackTrace();
             }
         }
