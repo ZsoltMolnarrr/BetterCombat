040602293ef3f8c29c8b94c877a588bd2b9661b0
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2022-08-17 14:46:02 +0200

    Add item model predicate

--- src/main/java/net/bettercombat/client/BetterCombatClient.java
+++ src/main/java/net/bettercombat/client/BetterCombatClient.java
@@ -3,6 +3,7 @@ package net.bettercombat.client;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import me.shedaniel.autoconfig.serializer.PartitioningSerializer;
+import net.bettercombat.BetterCombat;
 import net.bettercombat.client.animation.FirstPersonRenderHelper;
 import net.bettercombat.config.ClientConfig;
 import net.bettercombat.config.ClientConfigWrapper;
@@ -13,8 +14,10 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.item.ModelPredicateProviderRegistry;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
 
 @Environment(EnvType.CLIENT)
 public class BetterCombatClient implements ClientModInitializer {
@@ -39,6 +42,10 @@ public class BetterCombatClient implements ClientModInitializer {
         if (FabricLoader.getInstance().isModLoaded("firstperson")) {
             FirstPersonRenderHelper.isFeatureEnabled = false;
         }
+
+        ModelPredicateProviderRegistry.register(new Identifier(BetterCombat.MODID, "loaded"), (stack, world, entity, seed) -> {
+            return 1.0F;
+        });
     }
 
     private void registerKeyBindings() {
