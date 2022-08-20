fc985d459cadd9c4455699c8b4c73b56793ac821
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2022-08-20 12:38:45 +0200

    Improve dependency references in build.gradle files

--- forge/src/main/java/ca/lukegrahamlandry/bettercombat/BetterCombatForge.java
+++ forge/src/main/java/ca/lukegrahamlandry/bettercombat/BetterCombatForge.java
@@ -10,8 +10,6 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
-import java.util.List;
-
 @Mod(BetterCombat.MODID)
 public class BetterCombatForge {
     public static DeferredRegister<SoundEvent> SOUNDS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, BetterCombat.MODID);
