8314be137d7209e969a4eae46489cbc302fd927f
PeterTvedt <petertvedt01@gmail.com>
2022-10-03 17:31:36 -0400

    Fix LeftClickEmpty event not being fired #127

--- forge/src/main/java/net/bettercombat/forge/PlatformClientImpl.java
+++ forge/src/main/java/net/bettercombat/forge/PlatformClientImpl.java
@@ -1,11 +1,17 @@
 package net.bettercombat.forge;
 
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 
 import java.util.List;
 
 public class PlatformClientImpl {
     public static void registerKeyBindings(List<KeyBinding> keyBindings) {
         // Do nothing, Forge asks us to register
+    }
+    public static void onEmptyLeftClick(PlayerEntity player) {
+        MinecraftForge.EVENT_BUS.post(new PlayerInteractEvent.LeftClickEmpty(player));
     }
 }
