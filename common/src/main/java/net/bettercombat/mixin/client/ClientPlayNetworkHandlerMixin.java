ce7474a63d1b986f83ebfd1377064478510da140
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2023-02-25 19:32:50 +0100

    Fix overfiltering particles #198

--- common/src/main/java/net/bettercombat/mixin/client/ClientPlayNetworkHandlerMixin.java
+++ common/src/main/java/net/bettercombat/mixin/client/ClientPlayNetworkHandlerMixin.java
@@ -3,6 +3,7 @@ package net.bettercombat.mixin.client;
 import net.bettercombat.client.BetterCombatClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.packet.s2c.play.ParticleS2CPacket;
+import net.minecraft.particle.ParticleTypes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,7 +13,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ClientPlayNetworkHandlerMixin {
     @Inject(method = "onParticle", at = @At("HEAD"), cancellable = true)
     private void onParticle_Pre(ParticleS2CPacket packet, CallbackInfo ci) {
-        if(!BetterCombatClient.config.isSweepingParticleEnabled) {
+        if(!BetterCombatClient.config.isSweepingParticleEnabled
+                && packet.getParameters().getType().equals(ParticleTypes.SWEEP_ATTACK)) {
             ci.cancel();
         }
     }
