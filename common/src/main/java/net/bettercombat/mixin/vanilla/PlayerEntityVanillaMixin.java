9a36a75079029d34d6875c98808513462e3ec87a
Dave <26723989+Whitcome@users.noreply.github.com>
2024-02-26 17:38:23 -0600

    Fix other player's animations

--- common/src/main/java/net/bettercombat/mixin/vanilla/PlayerEntityVanillaMixin.java
+++ common/src/main/java/net/bettercombat/mixin/vanilla/PlayerEntityVanillaMixin.java
@@ -10,6 +10,8 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.Box;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -26,17 +28,8 @@ public abstract class PlayerEntityVanillaMixin extends LivingEntityVanillaMixin 
 
     @Shadow public abstract void attack(Entity target);
 
-    @Unique
-    private PlayerEntity getPlayer() {
-        var clientWorld = MinecraftClient.getInstance().world;
-        if (clientWorld == null) return null;
-        return (PlayerEntity)clientWorld.getEntityById(getEntity().getId());
-    }
-
-    @Unique
-    private Entity getEntity() {
-        return (Entity)((Object)this);
-    }
+    // Small correction to prevent animations while mining. Other players only.
+    @Unique private static final double PLAYER_REACH_CORRECTION = 0.1777;
 
     @Override
     protected void swingHand(CallbackInfo ci) {
@@ -47,7 +40,7 @@ public abstract class PlayerEntityVanillaMixin extends LivingEntityVanillaMixin 
         var player = getPlayer();
         if (player == null) return;
 
-        if (player.getAttackCooldownProgress(0) > 0) {
+        if (playerIsMining(player)) {
             comboCount = 0;
             var downWind = (int)Math.round(PlayerAttackHelper.getAttackCooldownTicksCapped(player) * (1 - 0.5 * BetterCombat.config.upswing_multiplier));
             ((PlayerAttackAnimatable) player).stopAttackAnimation(downWind);
@@ -91,5 +84,34 @@ public abstract class PlayerEntityVanillaMixin extends LivingEntityVanillaMixin 
         if (playerMainHandStack == null || playerMainHandStack.getItem() != itemLastAttackedWith) {
             comboCount = 0;
         }
+    }
+
+    @Unique
+    private PlayerEntity getPlayer() {
+        var clientWorld = MinecraftClient.getInstance().world;
+        if (clientWorld == null) return null;
+        return (PlayerEntity)clientWorld.getEntityById(getEntity().getId());
+    }
+
+    @Unique
+    private Entity getEntity() {
+        return (Entity)((Object)this);
+    }
+
+    @Unique
+    private boolean playerIsMining(PlayerEntity player) {
+        // Client player animations
+        var clientPlayer = MinecraftClient.getInstance().player;
+        if (clientPlayer != null && player.getUuid().equals(clientPlayer.getUuid())) {
+            return clientPlayer.getAttackCooldownProgress(0) > 0;
+        }
+
+        // Other player animations
+        var playerBuildReach = (player.isCreative() ? 5 : 4.5) + PLAYER_REACH_CORRECTION;
+        var playerCrosshairReach = player.raycast(playerBuildReach, 1.0F, false);
+        var attackableEntitiesInCrosshair = player.getWorld()
+                .getOtherEntities(player, new Box(player.getEyePos(), playerCrosshairReach.getPos()))
+                .stream().anyMatch(Entity::isAttackable);
+        return !attackableEntitiesInCrosshair && playerCrosshairReach.getType() == HitResult.Type.BLOCK;
     }
 }
