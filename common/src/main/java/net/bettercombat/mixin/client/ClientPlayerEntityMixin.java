66e01729b5706f5e43dad8e01d5219c036fcd592
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2025-04-24 18:27:46 +0200

    Fix movement (input) speed modifier

--- common/src/main/java/net/bettercombat/mixin/client/ClientPlayerEntityMixin.java
+++ common/src/main/java/net/bettercombat/mixin/client/ClientPlayerEntityMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayerEntity.class)
 public class ClientPlayerEntityMixin {
-    @Inject(method = "tickMovement", at = @At(value = "INVOKE", shift = At.Shift.AFTER))
+    @Inject(method = "tickMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/input/Input;tick()V", shift = At.Shift.AFTER))
     private void tickMovement_ModifyInput(CallbackInfo ci) {
         var config = BetterCombatMod.config;
         var multiplier = Math.min(Math.max(config.movement_speed_while_attacking, 0.0), 1.0);
