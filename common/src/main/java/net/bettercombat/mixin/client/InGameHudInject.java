f177379d3d8c44f776e92ba6a82df9267f08d6ab
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2025-04-24 19:06:07 +0200

    Fix crosshair coloring issues

--- common/src/main/java/net/bettercombat/mixin/client/InGameHudInject.java
+++ common/src/main/java/net/bettercombat/mixin/client/InGameHudInject.java
@@ -1,41 +1,40 @@
 package net.bettercombat.mixin.client;
 
-import com.mojang.blaze3d.systems.RenderSystem;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.bettercombat.api.MinecraftClient_BetterCombat;
 import net.bettercombat.client.BetterCombatClientMod;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.render.RenderTickCounter;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.ColorHelper;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.function.Function;
 
 @Mixin(InGameHud.class)
 public abstract class InGameHudInject {
-    @Inject(method = "renderCrosshair", at = @At(value = "INVOKE"))
-    private void pre_renderCrosshair(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
-        if (BetterCombatClientMod.config.isHighlightCrosshairEnabled) {
-            setShaderForHighlighting();
-        }
-    }
+    @WrapOperation(
+            method = "renderCrosshair",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Ljava/util/function/Function;Lnet/minecraft/util/Identifier;IIII)V"))
+    private void renderCrosshair_WrapOperation(DrawContext context, Function<Identifier, RenderLayer> renderLayers, Identifier sprite, int x, int y, int width, int height, Operation<Void> original) {
+        if (BetterCombatClientMod.config.isHighlightCrosshairEnabled
+            && ((MinecraftClient_BetterCombat) MinecraftClient.getInstance()).hasTargetsInReach()) {
+            float alpha = 0.5F;
 
-    @Inject(method = "renderCrosshair", at = @At(value = "TAIL"))
-    private void post_renderCrosshair(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-    }
-
-    @Unique
-    private void setShaderForHighlighting() {
-        if (((MinecraftClient_BetterCombat) MinecraftClient.getInstance()).hasTargetsInReach()) {
             var color = BetterCombatClientMod.config.hudHighlightColor;
             float red = ((float) ((color >> 16) & 0xFF)) / 255F;
             float green = ((float) ((color >> 8) & 0xFF)) / 255F;
             float blue = ((float) (color & 0xFF)) / 255F;
-            float alpha = 0.5F;
-            RenderSystem.setShaderColor(red, green, blue, alpha);
+
+            int colorARGB = ColorHelper.fromFloats(alpha, red, green, blue);
+
+            context.drawGuiTexture(renderLayers, sprite, x, y, width, height, colorARGB);
+        } else {
+            original.call(context, renderLayers, sprite, x, y, width, height);
         }
     }
 }
