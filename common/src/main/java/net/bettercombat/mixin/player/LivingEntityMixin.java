8f908e4ea58909d15f58e7251a1ec13177b4448e
Zsolt Molnar <zsolt.molnarrr@gmail.com>
2025-12-13 23:24:54 +0100

    Restore dual wielding

--- common/src/main/java/net/bettercombat/mixin/player/LivingEntityMixin.java
+++ common/src/main/java/net/bettercombat/mixin/player/LivingEntityMixin.java
@@ -1,17 +1,23 @@
 package net.bettercombat.mixin.player;
 
+import net.bettercombat.logic.InventoryUtil;
 import net.bettercombat.logic.PlayerAttackHelper;
 import net.bettercombat.logic.PlayerAttackProperties;
+import net.bettercombat.logic.WeaponRegistry;
 import net.bettercombat.logic.knockback.ConfigurableKnockback;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.entry.RegistryEntry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import static net.minecraft.entity.EquipmentSlot.OFFHAND;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin implements ConfigurableKnockback {
@@ -47,5 +53,33 @@ public class LivingEntityMixin implements ConfigurableKnockback {
     @ModifyVariable(method = "takeKnockback", at = @At("HEAD"), ordinal = 0, argsOnly = true)
     public double takeKnockback_HEAD_changeStrength(double knockbackStrength) {
         return knockbackStrength * customKnockbackMultiplier_BetterCombat;
+    }
+
+
+    @Inject(method = "getEquippedStack", at = @At("HEAD"), cancellable = true)
+    public void getEquippedStack_Pre(EquipmentSlot slot, CallbackInfoReturnable<ItemStack> cir) {
+        if (((Object)this) instanceof PlayerEntity player) {
+            var mainHandHasTwoHanded = false;
+            var mainHandStack = ((PlayerEntityAccessor) this).getInventory().getSelectedStack();
+            var mainHandAttributes = WeaponRegistry.getAttributes(mainHandStack);
+            if (mainHandAttributes != null && mainHandAttributes.isTwoHanded()) {
+                mainHandHasTwoHanded = true;
+            }
+
+            var offHandHasTwoHanded = false;
+            var offHandStack = InventoryUtil.getOffHandSlotStack(player);
+            var offHandAttributes = WeaponRegistry.getAttributes(offHandStack);
+            if(offHandAttributes != null && offHandAttributes.isTwoHanded()) {
+                offHandHasTwoHanded = true;
+            }
+
+            if (slot == OFFHAND) {
+                if (mainHandHasTwoHanded || offHandHasTwoHanded) {
+                    cir.setReturnValue(ItemStack.EMPTY);
+                    cir.cancel();
+                    return;
+                }
+            }
+        }
     }
 }
